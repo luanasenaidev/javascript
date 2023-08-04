4f58e50ee1af7d04b78f67a1bd6deef43f5ebfd6
luanasenaidev <luana.senaidev@gmail.com>
2023-08-04 16:07:57 -0300

    ate_string

---
+++ exemplos/2_TIPOS_DE_DADOS/4_string/script.js
@@ -0,0 +1,9 @@
+console.log("Meu nome é Luana.");
+console.log('Desenvolvimento é no Senai');
+
+// Template literais (crase)
+// Usar shift
+console.log(`teste`);
+
+console.log(typeof "Infinity")
+console.log(typeof Infinity)
