1b7e80b7d4dd72e6b9091bca710a3bc3d3d548f9
luanasenaidev <luana.senaidev@gmail.com>
2023-09-28 10:56:58 -0300

    condicionais e loops

--- exemplos/06-Condicionais/condicionais.js
+++ exemplos/06-Condicionais/condicionais.js
@@ -4,16 +4,24 @@ document.write('<h2 style="color: blue;">Condicional encadeada</h2><br>');
 
 /* if/else, else if */
 let nome = "Ignacio";
-let idade = 41;
 
 // Verificar idade da pessoa
 
 
 // Condicional encadeada
 let mensagem;
+let idade =16;
 
 
-// 1ª Digitação (Aqui)
+
+if(idade >= 18) {
+    mensagem = ("Você é maior de idade!");
+}
+else{
+    mensagem = ("Você é menor de idade!");
+}
+
+
 
 
 console.log(mensagem);
@@ -26,7 +34,17 @@ document.write('<h2 style="color: blue;">Condicional aninhada</h2><br>');
 
 
 
-// 2ª Digitação (Aqui)
+if (idade >=60) {
+    mensagem = "É idoso";
+} else if (idade >=18) {
+    mensagem = "É adulto, mas não é idoso";
+} else {
+    if (idade >=12 && idade <18) {
+        mensagem = "É adolescente.";
+    } else {
+        mensagem = "É baby."
+    }
+}
 
 
 document.write(`${nome}, ${mensagem} <br>`); // Mostra a tela (Navegador)
@@ -42,8 +60,15 @@ var n1 = 5.7;
 var n2 = 9;
 
 
-// 3ª Digitação (Aqui)
+media = (n1 + n2) /2;
 
+if (media >=7) {
+    situacao = 'Aprovado!';
+} else {
+    situacao = 'Reprovado';
+}
+// Operador Ternário
+    // situacao =  media >=7 'Aprovado!' :'Reprovado!';
 
 document.write(`Média: ${media}<br> `)
 document.write(`Status: ${situacao}<br>`); // Mostra a tela (Navegador)
@@ -64,10 +89,20 @@ x -> opçao desconhecida, direcionar para um humano */
 let opcao = 4;
 let textoOpcao;
 
-
-
-// 4ª Digitação (Aqui)
-
+switch(opcao) {
+    case 1:
+        textoOpcao = 'Legal, o que deseja saber';
+        break;
+    case 2:
+        textoOpcao = 'Que pena, o que aconteceu?';
+        break;
+    case 3:
+        textoOpcao = 'Certo, qual a dúvida?';
+        break;
+    case 4:
+        textoOpcao = 'Hum, não entendi...vou tranferir';
+        break;
+}
 
 document.write(`Você escolheu a opção ${opcao} <br>`);
 document.write(`${textoOpcao} <br>`);
@@ -80,9 +115,11 @@ document.write('<h2 style="color: blue;">Operadores de comparação</h2><br>');
 // Operadores de comparação
 
 
-// 5ª Digitação (Aqui)
+let a = 10;
+let b = '10';
+
 
 
 // == se é igual/ === estritamente igual (até tipo de dados)
-document.write(a == b); // True ou false
+document.write(a === b); // True ou false
 
--- exemplos/07-Loops/loops.js
+++ exemplos/07-Loops/loops.js
@@ -9,7 +9,12 @@ document.write('<h2 style="color: blue;">Loop While (Checa a condição e então
 // Exemplo 1: while (Checa a condição e então executa)
 
 
-// 1ª Digitação (Aqui)
+let i = 1;
+while (i <= 5) {
+    document.write(`O valor de i é: <strong>${i}</strong><br>`);
+    // i++;
+    i = i + 1;
+}
 
 
 document.write(`_____________________________________________<br>`);
@@ -19,7 +24,11 @@ document.write('<h2 style="color: blue;">Loop Do/While (Ele executa pelo menos 1
 // Exemplo 2: do/while (Ele executa pelo menos 1 vez e checa a condição para ver se continua)
 
 
-// 2ª Digitação (Aqui)
+let j = 1;
+do {
+    document.write(`O valor de j é: <strong>${j}</strong><br>`);
+    j++;
+} while (j <=3);
 
 
 // Exemplo 3: for
@@ -30,7 +39,9 @@ document.write('<h2 style="color: blue;">Loop For (Executa um bloco com base em 
 // for (inicialização; condição; incremento/decremento) 
 
 
-// 3ª Digitação (Aqui)
+for (let k = 1; k <=8; k++) {
+    document.write(`O valor de k é:<strong>${k}</strong><br>`);
+}
 
 
 document.write(`_____________________________________________<br>`);
@@ -45,6 +56,8 @@ let carros = [
 // Cache do tamanho array
 let tamanho = carros.length;
 
-// 4ª Digitação (Aqui)
+for (let i = 0; i < tamanho; i++) {41
+    document.write(`O valor de carro [${i}] é: <strong>${carros[i]}</strong><br>`);
+}
 
 
